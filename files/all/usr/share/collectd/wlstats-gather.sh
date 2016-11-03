f2a1b5f8c1013c349d49969f945bfd8bcf9ddde2
Petko Bordjukov <bordjukov@gmail.com>
2016-11-03 04:59:34 +0200

    Fix a bug in wlstats gatherer

--- files/all/usr/share/collectd/wlstats-gather.sh
+++ files/all/usr/share/collectd/wlstats-gather.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/share/collectd/wlstats_gatherer.sh "${COLLECTD_HOSTNAME:-localhost}" "${COLLECTD_INTERVAL:-10}"
+sudo /usr/share/collectd/wlstats-gatherer.sh "${COLLECTD_HOSTNAME:-localhost}" "${COLLECTD_INTERVAL:-10}"
